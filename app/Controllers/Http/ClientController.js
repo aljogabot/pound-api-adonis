a2f2829efb974807683e31fbe8868e25942a0e9d
Aljo Victor Gabot <aljo.gabot@gmail.com>
2019-08-20 04:21:05 +0800

    Added Pagination on the ClientController

--- app/Controllers/Http/ClientController.js
+++ app/Controllers/Http/ClientController.js
@@ -8,8 +8,20 @@ class ClientController {
         return { clients: await Client.all() }
     }
 
-    async listWithPagination() {
+    async listWithPagination({ request }) {
+        const paginationParams = request.all()
+        let clientQuery = Client.query()
 
+        const orderBy = request.input('sort_by', 'created_at')
+        const orderDirection = request.input('sort_direction', 'desc')
+        const rowsPerPage = request.input('rows_per_page', 10)
+
+        if (paginationParams.search.length > 0) {
+            clientQuery.where('first_name', 'like', `%${paginationParams.search}%`)
+                .orWhere('last_name', 'like', `%${paginationParams.search}%`)
+        }
+
+        return await clientQuery.orderBy(orderBy, orderDirection).paginate(paginationParams.page, rowsPerPage)
     }
 
 }

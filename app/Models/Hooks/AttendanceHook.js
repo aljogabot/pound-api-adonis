31f218b21d550c0d153969eb4f2463d3fb22bae0
Aljo Victor Gabot <aljo.gabot@gmail.com>
2019-08-24 17:14:28 +0800

    Clean Hooks

--- app/Models/Hooks/AttendanceHook.js
+++ app/Models/Hooks/AttendanceHook.js
@@ -17,29 +17,3 @@ AttendanceHook.attachRelatedModelsOnFetch = async (attendances) => {
         attendance.total_purchases = totalAmount.toFixed(2)
     }))
 }
-
-
-
-
-
-
-
-
-
-
-
-// 'use strict'
-
-// const ClientHook = exports = module.exports = {}
-
-// ClientHook.attachRelatedModelsOnFind = async (client) => {
-//     const currentSubscription = await client.currentClientSubscription().fetch()
-//     client.has_valid_subscription = currentSubscription ? currentSubscription.toJSON().active : false
-
-//     const currentMembership = await client.currentClientMembership().fetch()
-//     client.has_valid_membership = currentMembership ? currentMembership.toJSON().active : false
-// }
-
-// ClientHook.attachCurrentClientSubscriptionOnClients = async (clients) => {
-//     await Promise.all(clients.map(ClientHook.attachRelatedModelsOnFind))
-// }

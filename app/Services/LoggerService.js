a4466de1e1a657694904491ee1b822960d959665
Aljo Victor Gabot <aljo.gabot@gmail.com>
2019-08-27 04:23:04 +0800

    Fix LoggerService

--- app/Services/LoggerService.js
+++ app/Services/LoggerService.js
@@ -4,9 +4,14 @@ const Logger = use('App/Models/Logger')
 
 class LoggerService {
 
-    static push (data) {
-        const { action, table, model, description, user_id } = data
-        Logger.fill(data)
+    static push (data, auth) {
+        const { action, table, model, description } = data
+        const loggerDataObject = new Logger
+        loggerDataObject.merge(data)
+
+        loggerDataObject.user_id = user.id
+
+        loggerDataObject.save()
     }
 
 }
